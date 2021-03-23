d4fdde18e20dcae19e1d40639bae907a38e9ff52
mohamedehab69 <79059365+mohamedehab69@users.noreply.github.com>
2021-03-23 08:41:42 -1200

    Add files via upload

---
+++ my_packages/Operations/Math.py
@@ -0,0 +1,9 @@
+def get_average(list):
+    sum=0
+    for i in list :
+        sum=sum+i
+
+    avg=sum/len(list)
+    print(avg)
+
+
---
+++ my_packages/Transformers/StringTransformer.py
@@ -0,0 +1,8 @@
+def string_reverse():
+     string=input("enter any string :")
+     print(string[::-1])
+
+def string_captalize():
+     string=input("enter any string :")
+     print(string.capitalize())
+     
